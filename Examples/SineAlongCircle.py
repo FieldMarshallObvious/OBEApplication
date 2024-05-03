73cf83b7b48556ff1e864456564c9ffe618c4c9c
FieldMarshallObvious <noahdelangel@gmail.com>
2024-05-03 00:51:28 -0500

    Made progress bar and worked on sin
    Sine has decent trace before 1pi, but then all goes wrong after that --> I think it has to do with the rate function, somehow it needs to change based on the sine function
    
    Progress bar --> Made remedial progress bar, actaully needed for application

--- Examples/SineAlongCircle.py
+++ Examples/SineAlongCircle.py
@@ -1,50 +1,155 @@
 from manim import *
+from scipy.misc import derivative
 
 class SineAlongCircle(Scene):
     def construct(self):
         # Define the axes
         ax = Axes(
             x_range=[-1, 8], 
-            y_range=[-3, 3],
+            y_range=[-4, 4],
             tips=True,
             axis_config={"include_numbers": True, "tip_shape": StealthTip},
         )
         
         # Create the circle
+        sin_curve = ax.plot(lambda x: np.sin(x), color=RED)
         sin_circle = Circle(radius=1, color=WHITE).move_to(ax.c2p(0,0) + 1*LEFT)
-        
         # Create the dot
         dot = Dot(color=RED).move_to(sin_circle.get_critical_point(RIGHT))
 
-        sin_curve = ax.plot(lambda x: np.sin(x), color=RED)
+        self.curve_start = np.array([-4.5,0,0])
+        self.t_offset = 0
+        
 
-        path_curve = ax.plot(lambda x: np.sin(x), color=YELLOW, fill_opacity=0, x_range=[0, 7])
-        path_curve.z_index = 0
-        sin_curve.z_index = 1
         
         def update_dot(mob, dt):
-            rate = (2*PI)/15  # Speed control
-            # Update the parameter t, decrease to move backwards
-            mob.t += rate * dt
+            rate_of_change = dt * abs(np.sin(dt)) * 30
+            if self.t_offset > 1 * np.pi:
+                rate_of_change *= 0.9
 
-            # Ensure t stays within the valid range
-            mob.t %= 2 * PI
-
-            # Update the position of the dot
-            mob.move_to(path_curve.point_from_proportion(mob.t / (2 * PI)))
-            
-        dot.t = 0
-        # Initialize the offset for the dot's rotation
-        self.t_offset =0
-
-        self.add(ax, sin_circle, sin_curve, dot, path_curve)
-        self.add(dot.add_updater(update_dot))
-
-        self.play(sin_circle.animate.shift(6.5*RIGHT), run_time=8, rate_func=linear)
-        self.play(sin_circle.animate.shift(6.5*LEFT), run_time=10, rate_func=linear)
+            self.t_offset += rate_of_change
+            mob.move_to(sin_circle.point_from_proportion((self.t_offset/ TAU) % 1))
         
+        self.curve = VGroup()
+        self.curve.add(Line(self.curve_start,self.curve_start))
+
+        def get_curve():
+            last_line = self.curve[-1]
+            x = dot.get_center()[0]
+            y = dot.get_center()[1]
+            new_line = Line(last_line.get_end(),np.array([x,y,0]), color=YELLOW_D)
+            self.curve.add(new_line)
+
+            return self.curve
+        
+        def move_circle(mob, dt):
+            speed_factor = 1 if self.t_offset < 1 * np.pi else abs(np.sin(dt)) * 25
+            new_x = sin_circle.get_center()[0] + dt * speed_factor
+            new_y = sin_circle.get_center()[1]
+            new_position = [new_x, new_y, sin_circle.get_center()[2]]
+            mob.move_to(new_position)
+
+        
+        sine_curve_line = always_redraw(get_curve)
+            
+        # Initialize the offset for the dot's rotation
+        self.t_offset = 0
+
+        self.add(ax, sin_circle, dot, sine_curve_line, sin_curve)
+        self.add(dot.add_updater(update_dot))
+        self.add(sin_circle.add_updater(move_circle))
+
+        self.wait(24)        
         # Remove updater to avoid conflict with other animations or renders
         dot.remove_updater(update_dot)
+        sine_curve_line.remove_updater(move_circle)
 
         # End the scene
         self.wait()
+
+class Sine_Curve(Scene):
+    def construct(self):
+        self.show_axis()
+        self.show_circle()
+        self.move_dot_and_draw_curve()
+
+        self.wait()
+
+    def show_axis(self):
+        x_start = np.array([-6,0,0])
+        x_end = np.array([6,0,0])
+
+        y_start = np.array([-4,-2,0])
+        y_end = np.array([-4,2,0])
+
+        x_axis = Line(x_start, x_end)
+        y_axis = Line(y_start, y_end)
+
+        self.add(x_axis, y_axis)
+        self.add_x_labels()
+
+        self.orgin_point = np.array([-4,0,0])
+        self.curve_start = np.array([-3,0,0])
+
+    def add_x_labels(self):
+        x_labels = [
+            MathTex(r'\pi'), MathTex(r'2 \pi'),
+            MathTex(r'3 \pi'), MathTex(r'4 \pi'),
+        ]
+
+        for i in range(len(x_labels)):
+            x_labels[i].next_to(np.array([-1+2*i,0,0]), DOWN )
+            self.add(x_labels[i])
+
+    def show_circle(self):
+        circle = Circle(radius=1)
+        circle.move_to(self.orgin_point)
+
+        self.add(circle)
+        self.circle = circle
+
+    def move_dot_and_draw_curve(self):
+        orbit = self.circle
+        orgin_point = self.orgin_point
+
+        dot = Dot(radius=0.08, color=YELLOW)
+        dot.move_to(orbit.point_from_proportion(0))
+        self.t_offset = 0
+        rate = 0.25
+
+        def go_around_circle(mob, dt):
+            self.t_offset += (dt * rate)
+            # print(self.t_offset)
+            mob.move_to(orbit.point_from_proportion(self.t_offset % 1))
+
+        def get_line_to_circle():
+            return Line(orgin_point, dot.get_center(), color=BLUE)
+
+        def get_line_to_curve():
+            x = self.curve_start[0] + self.t_offset * 4
+            y = dot.get_center()[1]
+            return Line(dot.get_center(), np.array([x,y,0]), color=YELLOW_A, stroke_width=2 )
+
+
+        self.curve = VGroup()
+        self.curve.add(Line(self.curve_start,self.curve_start))
+        def get_curve():
+            last_line = self.curve[-1]
+            x = self.curve_start[0] + self.t_offset * 4
+            y = dot.get_center()[1]
+            new_line = Line(last_line.get_end(),np.array([x,y,0]), color=YELLOW_D)
+            self.curve.add(new_line)
+
+            return self.curve
+
+        dot.add_updater(go_around_circle)
+
+        origin_to_circle_line = always_redraw(get_line_to_circle)
+        dot_to_curve_line = always_redraw(get_line_to_curve)
+        sine_curve_line = always_redraw(get_curve)
+
+        self.add(dot)
+        self.add(orbit, origin_to_circle_line, dot_to_curve_line, sine_curve_line)
+        self.wait(8.5)
+
+        dot.remove_updater(go_around_circle)
--- progress_bar.py
+++ progress_bar.py
@@ -1,13 +1,15 @@
 from manim import *
+import random
 
 class CustomProgressBar(Scene):
     def construct(self):
+        rectangle_height = 0.5
         # Create the outline of the progress bar
-        bar_outline = Rectangle(width=6, height=1, color=WHITE)
+        bar_outline = Rectangle(width=6, height=rectangle_height, color=WHITE)
         bar_outline.set_fill(BLACK, opacity=1)
 
         # Initialize the progress bar with a very small fill
-        initial_fill = Rectangle(width=0.1, height=1, color=WHITE)
+        initial_fill = Rectangle(width=0.1, height=rectangle_height, color=WHITE)
         initial_fill.set_fill(WHITE, opacity=1)
         initial_fill.set_stroke(width=0)
         initial_fill.next_to(bar_outline.get_left(), RIGHT, buff=0)
@@ -19,42 +21,46 @@ class CustomProgressBar(Scene):
         current_fill = initial_fill
 
         # Animate the progress bar
-        for i in range(1, 11):  # Example: 10 steps to fill the bar
-            new_width = 0.6 * i  # Incremental width
-            if new_width > 6:
-                new_width = 6
+        for i in range(1, 4):
+            fail_point = random.randint(1, 9) if i < 3 else 12
+            for j in range(1, 11):  # Example: 10 steps to fill the bar
+                new_width = 0.6 * j  # Incremental width
+                if new_width > 6:
+                    new_width = 6
 
-            new_fill = Rectangle(width=new_width, height=1, color=WHITE)
-            new_fill.set_fill(WHITE, opacity=1)
-            new_fill.set_stroke(width=0)
-            new_fill.next_to(bar_outline.get_left(), RIGHT, buff=0)
+                new_fill = Rectangle(width=new_width, height=rectangle_height, color=WHITE)
+                new_fill.set_fill(WHITE, opacity=1)
+                new_fill.set_stroke(width=0)
+                new_fill.next_to(bar_outline.get_left(), RIGHT, buff=0)
 
-            # Replace the old fill with the new one in the animation
-            self.play(Transform(current_fill, new_fill))
-            if new_width == 6:
-                break  # Stop if the bar is full
-        
-        # Determine if the progress completes or fails
-        success = True  # Change this to False to simulate failure
+                # Replace the old fill with the new one in the animation
+                self.play(Transform(current_fill, new_fill))
+
+                if j == fail_point:
+                    current_fill.set_fill(RED)
+                    current_fill.set_stroke(color=RED)
+                    bar_outline.set_stroke(color=RED)
+                    self.play(
+                        Wiggle(current_fill,scale_factor=1.2, rotation_angle=0.15, rotate_about_point=current_fill.get_center()),
+                        Wiggle(bar_outline,scale_factor=1.2, rotation_angle=0.15, rotate_about_point=bar_outline.get_center()),
+                    )
+                    self.play(Transform(current_fill, initial_fill))
+                    current_fill = initial_fill
+                    bar_outline.set_stroke(color=WHITE)
+                    break
 
 
-        if success:
-            current_fill.set_fill(GREEN)
-            current_fill.set_stroke(color=GREEN)
-            bar_outline.set_fill(GREEN)
-            bar_outline.set_stroke(color=GREEN) 
-        else:
-            current_fill.set_fill(RED)
-            current_fill.set_stroke(color=RED)
-            bar_outline.set_fill(RED)
-            bar_outline.set_stroke(color=RED) 
+                if new_width == 6:
+                    break  # Stop if the bar is full
+            
+        current_fill.set_fill(GREEN)
+        current_fill.set_stroke(color=GREEN)
+        bar_outline.set_fill(GREEN)
+        bar_outline.set_stroke(color=GREEN) 
 
         # If success, emphasize green
         self.play(
             Wiggle(current_fill,scale_factor=1.2, rotation_angle=0.15, rotate_about_point=current_fill.get_center()),
-            Wiggle(bar_outline,scale_factor=1.2, rotation_angle=0.15, rotate_about_point=current_fill.get_center()),
+            Wiggle(bar_outline,scale_factor=1.2, rotation_angle=0.15, rotate_about_point=bar_outline.get_center()),
         )
         self.wait(1)
-
-# To run this scene, use the following command in your terminal:
-# manim -pql your_script_name.py CustomProgressBar
